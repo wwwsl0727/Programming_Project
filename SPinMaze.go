50a1ef9e522607aabadd5fa4c078ee7d22b319ce
Shili Wang <sallyw0727@gmail.com>
2019-11-08 18:41:52 -0500

    calculate pressure for the maze

--- SPinMaze.go
+++ SPinMaze.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 )
 
 type Maze []*Node
@@ -16,6 +18,8 @@ type Node struct {
 type OrderedPair struct {
 	x, y float64
 }
+
+type Matrix [][]float64
 
 func main() {
 	fmt.Println("Maze!")
@@ -68,13 +72,13 @@ func main() {
 	N13.name, N14.name, N15.name, N16.name, N17.name, N18.name, N19.name = "N13", "N14", "N15", "N16", "N17", "N18", "N19"
 	var maze Maze
 	maze = append(maze, &N1, &N2, &N3, &N4, &N5, &N6, &N7, &N8, &N9, &N10, &N11, &N12, &N13, &N14, &N15, &N16, &N17, &N18, &N19)
-	fmt.Println("address of N1", maze[0])                            // &{[0xc000086180] 0 {0 0} N1}
-	fmt.Println("attributes of N1", *maze[0])                        //{[0xc000086180] 0 {0 0} N1}
-	fmt.Println("name of N1", (*maze[0]).name)                       //"N1"
-	fmt.Println(" address of neighbors of N1", (*maze[0]).neighbors) //[&N6] [0xc0000980f0]
-	address := (*maze[0]).neighbors[0]                               //&N6
-	fmt.Println("address of N6", address)                            //&N6
-	fmt.Println("name of N6", (*address).name)                       //"N6"
+	// fmt.Println("address of N1", maze[0])                            // &{[0xc000086180] 0 {0 0} N1}
+	// fmt.Println("attributes of N1", *maze[0])                        //{[0xc000086180] 0 {0 0} N1}
+	// fmt.Println("name of N1", (*maze[0]).name)                       //"N1"
+	// fmt.Println(" address of neighbors of N1", (*maze[0]).neighbors) //[&N6] [0xc0000980f0]
+	// address := (*maze[0]).neighbors[0]                               //&N6
+	// fmt.Println("address of N6", address)                            //&N6
+	// fmt.Println("name of N6", (*address).name)                       //"N6"
 
 	//Check if the intialization is right
 	//For each node
@@ -89,8 +93,361 @@ func main() {
 	//
 	// }
 
-	//Initialize Pij, Qij, Dij
-	//Compute Pij
-	//Compute Qij
-	//Compute Dij
+	numgen := 1
+
+	//Q := MazeEvolve(maze, numgen)
+	MazeEvolve(maze, numgen)
+}
+
+//MazeEvolve takes in maze as input and return the flow quantity matrix
+func MazeEvolve(maze Maze, numgen int) []Matrix {
+	N := len(maze)
+	// Initialize Pij, Qij, Dij
+	P := make([][]float64, numgen+1)
+	for i := range P {
+		P[i] = make([]float64, N)
+	}
+	//Q := make([]Matrix, numgen+1)
+	D := make([]Matrix, numgen+1)
+
+	//Initialize D[0]_ij=0.5, d= D[0]
+	D[0] = InitializeD(N)
+
+	//Compute the length of node i and its neighbor
+	L := ComputeLengthMatrix(maze)
+
+	//Evolve
+	for n := 1; n <= numgen; n++ {
+
+		//Compute Pij
+		//P[n] = ComputeP(D[n-1], L, P[n-1])
+		P[n], _ = ComputeP(D[n-1], L)
+		//Compute Qij
+
+		//Q[n] = ComputeQ(D[n-1], L, P[n])
+
+		//D[n]=ComputeD()
+	}
+	// fmt.Println("here")
+	fmt.Println(P[1])
+	return D
+}
+
+// func ComputeD()
+
+func ComputeQ(d, L Matrix, p []float64) Matrix {
+	N := len(d)
+
+	//initialize q
+	q := make(Matrix, N)
+	for r := range q {
+		q[r] = make([]float64, N)
+	}
+
+	for i := 0; i <= N-1; i++ {
+		for j := 0; j <= N-1; j++ {
+			if L[i][j] != 0 {
+				q[i][j] = (d[i][j] * (p[i] - p[j])) / L[i][j]
+			}
+		}
+	}
+
+	return q
+}
+
+/*func ComputeP(d, L Matrix, x []float64) []float64 {
+	N := len(d)
+	A := InitializePCoefficient(d, L)
+	b := make([]float64, N)
+	b[0] = -1
+	b[1] = 1
+	//ICCG
+
+	r := VectorMinus(b, DotVector(A, x)) //N*1 for p,r,b,x0, A N*N
+	fmt.Println("r,p", r)
+	p := r
+
+	rsold := dot2vector(r, r)
+	fmt.Println("rsold", rsold)
+	for i := 0; i <= 0; i++ {
+		Ap := DotVector(A, p)
+		fmt.Println("Ap", Ap)
+		alpha := rsold / dot2vector(p, Ap)
+		fmt.Println("Alpha", alpha)
+		x = VectorAdd(x, VectorMulConstant(p, alpha))
+		fmt.Println("x", x)
+		r := VectorMinus(r, VectorMulConstant(Ap, alpha))
+		fmt.Println("r", r)
+		rsnew := dot2vector(r, r)
+		fmt.Println("rsnew", rsnew)
+		if math.Sqrt(rsnew) < 1e-10 {
+			break
+		}
+		temp := VectorMulConstant(p, rsnew/rsold)
+		p = VectorAdd(r, temp)
+		rsold = rsnew
+	}
+	return x
+}
+func VectorMinus(a []float64, b []float64) []float64 {
+	m1 := len(a)
+	m2 := len(b)
+	if m1 != m2 {
+		panic("the given matrixs for addition have wrong dimension")
+	}
+	c := make([]float64, m1)
+	for i := 0; i <= m1-1; i++ {
+		c[i] = a[i] - b[i]
+	}
+	return c
+}
+func VectorMulConstant(a []float64, b float64) []float64 {
+	for i := 0; i <= len(a)-1; i++ {
+		a[i] *= b
+	}
+	return a
+}
+func VectorAdd(a []float64, b []float64) []float64 {
+	m1 := len(a)
+	m2 := len(b)
+	if m1 != m2 {
+		panic("the given matrixs for addition have wrong dimension")
+	}
+	c := make([]float64, m1)
+	for i := 0; i <= m1-1; i++ {
+		c[i] = a[i] + b[i]
+	}
+	return c
+}
+func dot2vector(a []float64, b []float64) float64 {
+	m1 := len(a)
+	m2 := len(b)
+	if m1 != m2 {
+		panic("the given matrixs for multiplification have wrong dimension")
+	}
+
+	result := 0.0
+	for i := 0; i <= m1-1; i++ {
+		result += a[i] * b[i]
+	}
+	return result
+}
+func DotVector(a Matrix, b []float64) []float64 {
+	m1 := len(a)
+	m2 := len(b)
+	n1 := len(a[0])
+	if m2 != n1 {
+		panic("the given matrixs for multiplification have wrong dimension")
+	}
+
+	result := make([]float64, m1)
+
+	for i := 0; i <= m1-1; i++ {
+		for k := 0; k <= n1-1; k++ {
+			result[i] += a[i][k] * b[k]
+		}
+	}
+	return result
+}
+func dot(a, b [][]float64) [][]float64 {
+	m1 := len(a)
+	m2 := len(b)
+	n1 := len(a[0])
+	n2 := len(b[1])
+
+	result := make([][]float64, m1)
+	for i := range result {
+		result[i] = make([]float64, n2)
+	}
+	if m2 != n1 {
+		panic("the given matrixs for multiplification have wrong dimension")
+	}
+
+	for i := 0; i <= m1-1; i++ {
+		for k := 0; k <= n2-1; k++ {
+			for j := 0; j <= n1-1; j++ {
+				result[i][k] += a[i][j] * b[j][k]
+			}
+		}
+	}
+	return result
+}
+func Transpose(a [][]float64) [][]float64 {
+	m := len(a)
+	n := len(a[0])
+
+	result := make([][]float64, n)
+	for i := range result {
+		result[i] = make([]float64, m)
+	}
+	for i := range a {
+		for j := range a[i] {
+			result[j][i] = a[i][j]
+		}
+	}
+	return result
+} */
+
+//Check if A is positive and linear dependent?
+//CalculateP takes in D and L matrix, P2=0. Solve the remaining n-1 P by n equations. Returning p matrix is a list of float64 variables.
+func ComputeP(d, L Matrix) ([]float64, error) {
+
+	//solve linear equations by guassian GaussianElimination : AX=b
+	//Initialize the faction matrix dij/lij, augment A by adding the b terms.
+
+	A := InitializePCoefficient(d, L)
+
+	// //P2=p[1]==0,delete the second row
+	// A = append(A[:1], A[2:]...)
+
+	// //next delete the second column because p2=0
+	for i := range A {
+		A[i] = append(A[i][:1], A[i][2:]...)
+	}
+
+	N := len(A)
+	M := len(A[0]) //N-1
+	fmt.Println("N", N)
+	fmt.Println("M", M)
+	//For all rows
+	i := 0
+	k := 0
+	for i <= N-1 && k <= M-1 {
+		/* Find the k-th pivot:*/
+		iMax := Argmax(i, k, A)
+
+		if A[iMax][k] == 0 {
+			/*no pivot in this column*/
+			k++
+		} else {
+			A[i], A[iMax] = A[iMax], A[i]
+
+			//for all rows below pivot, change their values
+			for r := i + 1; r <= N-1; r++ {
+				f := -A[r][k] / A[i][k]
+				//for all remaining nonzero elements in current row
+				for c := k + 1; c <= M-1; c++ {
+					A[r][c] += f * A[i][c]
+				}
+				//Fill 0 with the lower part of the pivot column
+				A[r][k] = 0
+			}
+		}
+		/*Increase pivot row and column*/
+		fmt.Println("i,Ai", i, A[i])
+		i++
+		k++
+	}
+
+	//retrive p from the upper triangular matrix
+	p := make([]float64, N)
+	for i := N - 2; i >= 0; i-- {
+		p[i] = A[i][M-1]
+		for j := i + 1; j <= M-1; j++ {
+			p[i] -= A[i][j] * p[j]
+		}
+		p[i] /= A[i][i]
+	}
+
+	for i := N - 1; i >= 2; i-- {
+		p[i] = p[i-1]
+	}
+	p[1] = 0.0
+	return p, nil
+}
+
+//i_max := argmax (i = h ... m, abs(A[i, k]))*/
+
+func Argmax(i, k int, A Matrix) int {
+	N := len(A)
+	max := A[i][k]
+	imax := i
+	for r := i + 1; r <= N-1; r++ {
+		if temp := A[r][k]; temp > max {
+			max = temp
+			imax = r
+		}
+	}
+	return imax
+}
+func InitializePCoefficient(d, L Matrix) Matrix {
+	N := len(d)
+	M := N + 1 //number of columns
+	//The dimention of A is N*(N+1),the extra column is for augment b terms.
+	A := make(Matrix, N)
+	for r := range A {
+		A[r] = make([]float64, M)
+	}
+
+	for i := 0; i <= N-1; i++ {
+		for j := 0; j <= M-1; j++ {
+			//For Dij/Lij
+			if j <= N-1 {
+				if L[i][j] != 0 && i != j {
+					A[i][j] = d[i][j] / L[i][j]
+					A[i][i] -= A[i][j]
+					//fmt.Println("i,Aii", i, A[i][i])
+				}
+			} else {
+				if i == 0 {
+					//For the starting point N1, b=A[0][N] =-1
+					A[i][j] = -1
+				}
+				//For the end point N2, b=b=A[1][N] = =1
+				if i == 1 {
+					A[i][j] = 1
+				}
+			}
+		}
+	}
+
+	return A
+}
+
+//ComputeLength takes in the maze and returns the length between the distance from the node to its neighbors
+func ComputeLengthMatrix(maze Maze) Matrix {
+	N := len(maze)
+
+	L := make(Matrix, N)
+	for r := range L {
+		L[r] = make([]float64, N)
+	}
+
+	//Calculate distances of one node to its all neighbors
+	for i := range maze {
+		//address of neighbors of Ni
+		neighbors := (*maze[i]).neighbors
+		for j := range neighbors {
+			//j is just the order of the neighbor in neighbors. To know its real index in the maze and L
+			// we need to extract it from the name, the real index is the indexj-1 N1->0
+			indexj, _ := strconv.Atoi((*(neighbors[j])).name[1:])
+			indexj--
+			//fmt.Println((*(neighbors[0])).name[1:])
+			l := ComputeLength(maze, i, j)
+			L[i][indexj], L[indexj][i] = l, l
+			// fmt.Println("length of node", i+1, "and node", indexj+1, "is", l)
+		}
+	}
+	return L
+}
+
+func ComputeLength(maze Maze, i, j int) float64 {
+	node := *maze[i]
+	nodeneighborj := *(node.neighbors[j])
+	return math.Abs(node.location.x-nodeneighborj.location.x) + math.Abs(node.location.y-nodeneighborj.location.y)
+}
+
+//Initialize D takes the length of D[0] and returns N*N matrix where value of each entry is 0.5
+func InitializeD(N int) Matrix {
+	d := make(Matrix, N)
+	for r := range d {
+		d[r] = make([]float64, N)
+	}
+
+	for r := range d {
+		for c := range d[r] {
+			d[r][c] = 0.5
+		}
+	}
+	return d
 }
