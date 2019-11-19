b7f4b14da9f2fcc22348319b34727a41facf36fc
Shili Wang <sallyw0727@gmail.com>
2019-11-18 23:55:20 -0500

    change the food sources and synthesize comparator

--- MultiAgent_Mold.go
+++ MultiAgent_Mold.go
@@ -14,7 +14,9 @@ func main() {
 	//intialization for 50% case
 	row := 200
 	col := 200
-	SensorArmLength := 7
+	sensorArmLength := 7
+	sensorAngle:=math.Pi / float64(4)
+	sensorDiagonalL := float64(5)*math.Sqrt(2)
 	depT := 5    //The quantity of trail deposited by an agent
 	dampT := 0.1 //Diffusion damping factor of trail
 	// filter for trail 3x3
@@ -25,7 +27,7 @@ func main() {
 	WN := 1 - WT
 
 	CN := 10.0   //The Chemo-Nutrient concentration of food
-	dampN := 0.2 //Diffusion damping factor of chemo- nutrient
+	// dampN := 0.2 //Diffusion damping factor of chemo- nutrient
 	// filter for nutrient 5x5
 
 	//motion counter >= RT, reproduction
@@ -39,18 +41,43 @@ func main() {
 		//for all boxes, intial foodChemo & trailChemo are 0.
 		//IsFood, IsAgent are false
 		row := make([]box, col)
-		row = generateAgent(row, SensorArmLength)
+		row = generateAgent(row, sensorArmLength,sensorDiagonalL,sensorAngle)
 		matrix[i] = row
 	}
-	//put three food spots on the Matrix
-	matrix[10][100].IsFood = true
-	matrix[10][100].foodChemo = CN //10
 
-	matrix[190][10].IsFood = true
-	matrix[190][10].foodChemo = CN //10
+	//put three food spots, each spot is a 3*3 matrix on the Matrix
+	// matrix[10][100].IsFood = true
+	// matrix[10][100].foodChemo = CN //10
+	//
+	// matrix[190][10].IsFood = true
+	// matrix[190][10].foodChemo = CN //10
+	//
+	// matrix[190][190].IsFood = true
+	// matrix[190][190].foodChemo = CN //10
 
-	matrix[190][190].IsFood = true
-	matrix[190][190].foodChemo = CN //10
+	//The center is 10,100
+	for i:=9;i<=11;i++{
+		for j:=99;j<=101;j++{
+			matrix[i][j].IsFood = true
+			matrix[i][j].foodChemo = CN //10
+		}
+	}
+
+	//The center is 190,10
+	for i:=189;i<=191;i++{
+		for j:=9;j<=11;j++{
+			matrix[i][j].IsFood = true
+			matrix[i][j].foodChemo = CN //10
+		}
+	}
+
+	//The center is 190,190
+	for i:=189;i<=191;i++{
+		for j:=189;j<=191;j++{
+			matrix[i][j].IsFood = true
+			matrix[i][j].foodChemo = CN //10
+		}
+	}
 }
 
 type multiAgentMatrix [][]box
@@ -64,22 +91,25 @@ type box struct {
 
 type Agent struct {
 	motionCounter int // initial 0
-	direction     int // direction could be 1 - 8, represent 8 direction
+	direction     float64 // direction could be the multiple of pi/4
 	sensorLength  int // 7 in 50% agent case
+	sensorDiagonalL float64 // float64(5)*math.Sqrt(2) for sensor in diagnol of axix
 }
 
-func generateAgent(row []box, SensorArmLength int) []box {
+func generateAgent(row []box, sensorArmLength int,sensorDiagonalL,sensorAngle float64) []box {
 
 	for i := range row {
 		n := rand.Intn(2)                   //50% chance of having an agent in the box
-		randomDirection := rand.Intn(8) + 1 //This create a random direction from 1 - 8
+		// randomDirection := rand.Intn(8) + 1 //This create a random direction from 1 - 8
+		randomDirection := float64(rand.Intn(8) + 1) * sensorAngle //This create a random direction from pi/4 -2pi
 		if n == 0 {
 			row[i].IsAgent = true
 			//create a new agent and points to it
 			var oneAgent Agent
 			oneAgent.motionCounter = 0
 			oneAgent.direction = randomDirection
-			oneAgent.sensorLength = SensorArmLength
+			oneAgent.sensorLength = sensorArmLength
+			oneAgent.sensorDiagonalL = sensorDiagonalL
 			row[i].agent = &oneAgent
 		}
 		// if n == 1, no agent
@@ -87,78 +117,103 @@ func generateAgent(row []box, SensorArmLength int) []box {
 	return row
 }
 
-//Compare three sensors measure, change a agent's direction
-func (matrix multiAgentMatrix) SynthesisComparator(row, col int, WT, WN float64) {
+//Compare two sensors measure, change a agent's direction
+func (matrix multiAgentMatrix) SynthesisComparator(row, col int, WT, WN,sensorAngle float64) {
+	//exceptions
+	numRows := len(matrix)
+	numCols := len(matrix[0])
+	if row < 0 || col < 0 || row >= numRows || col >= numCols {
+		panic("the given row and col is out of bound")
+	}
+
 	if matrix[row][col].IsAgent == false {
 		panic("This box has no Agent!")
 	}
 
-	sensorL := matrix[row][col].agent.sensorLength
+	agentDirection:=matrix[row][col].agent.direction
 
-	//Check if three sensors are in field and calculate the socre
-	var sensor1Score, sensor2Score, sensor3Score float64
+	//For sensors on the x and y axis, sensorLength is 7
+	L := matrix[row][col].agent.sensorLength
 
-	//sensor 1
-	if InField(matrix, row, col) {
-		sensor1 := matrix[row-sensorL][col-sensorL]
-		sensor1Score = calculateScore(sensor1, WT, WN)
-	} else {
-		sensor1Score = 0
+	//For sensors on the diagonal axis of xy plane, in order to get the location as integers, sensorlength is 5*sqart(2)
+	DiagonalL := matrix[row][col].agent.sensorDiagonalL
+
+
+	// The direction of the left,right sensor is based on the current direction of the agent.
+	leftx,lefty,rightx,righty := CalculateSensorLocation(agentDirection, sensorAngle, DiagonalL, row, col, L)
+
+	//Get sample chemoattractant values from sensors.
+	// If left sensor and right sensor are all in the matrix
+	if InField(numRows,numCols, leftx, lefty)  && InField(numRows,numCols, rightx, righty){
+		sensorLeft := matrix[leftx][lefty]
+		FL := calculateScore(sensorLeft, WT, WN)
+		sensorRight := matrix[rightx][righty]
+		FR := calculateScore(sensorRight, WT, WN)
+		if FL<FR{
+			//rotate right
+			matrix[row][col].agent.direction -=sensorAngle
+		}else if FL>FR{
+				matrix[row][col].agent.direction +=sensorAngle
+		}else{
+			//random choose left and right
+			if rand.Intn(2)==0{
+					matrix[row][col].agent.direction +=sensorAngle
+			}else{
+					matrix[row][col].agent.direction -=sensorAngle
+			}
+		}
+	}else if InField(numRows,numCols, leftx, lefty)== false && InField(numRows,numCols, rightx, righty){
+		// If the left sensor is out of bound while the right sensor is in, turn right
+		matrix[row][col].agent.direction -=sensorAngle
+	}else if InField(numRows,numCols, leftx, lefty) && InField(numRows,numCols, rightx, righty)==false{
+		//Otherwise, it turns left
+			matrix[row][col].agent.direction +=sensorAngle
+	}else{
+		//If both side is out of bound, the agent turns back
+		matrix[row][col].agent.direction += math.Pi
 	}
 
-	//sensor 2
-	if InField(matrix, row, col) {
-		sensor2 := matrix[row-sensorL][col]
-		sensor2Score = calculateScore(sensor2, WT, WN)
-	} else {
-		sensor2Score = 0
-	}
-
-	//sensor 3
-	if InField(matrix, row, col) {
-		sensor3 := matrix[row-sensorL][col+sensorL]
-		sensor3Score = calculateScore(sensor3, WT, WN)
-	} else {
-		sensor3Score = 0
-	}
-
-	//Find the biggest of three scores and change agent's direction
-	max := FindMax(sensor1Score, sensor2Score, sensor3Score)
-	if max == 1 {
-		matrix[row][col].agent.direction -= 1
-	}
-	// if max == 2 {
-	// 	matrix[row][col].agent.direction
-	// }
-	if max == 3 {
-		matrix[row][col].agent.direction += 1
-	} else {
-		matrix[row][col].agent.direction += 1
-	}
 }
+
+//Calculate the location of left and right sensors.
+ func CalculateSensorLocation(agentDirection,sensorAngle,DiagonalL float64,row,col,L int)(int,int,int,int){
+	//Initialize the location of left, forward and right sensors
+		 var leftx int
+		 var lefty int
+		 var rightx int
+		 var righty int
+
+	flag := int(agentDirection/sensorAngle)
+	leftDirection:=agentDirection+ sensorAngle
+	rightDirection:=agentDirection- sensorAngle
+
+	//a is the coefficient for left and right arms; b is the coefficient for forward arms
+	//a,b =L/DiagonalL
+	var a float64
+	//Check whether the forward direction of agent is +-pi/4;+-3pi/4;+-5pi/4;+-7pi/4;;
+	//If so, a=L;leftx = row+a*cos(leftDirection); lefty= row+a*sin(leftDirection)
+	//rightx = row+a*cos(rightDirection); righty= row+a*sin(rightDirection)
+	//Else, exchange a=DiagonalL
+	if flag % 2 !=0{
+		 a=float64(L)
+	}else{
+		a=DiagonalL
+	}
+	leftx=row+int(a*math.Cos(leftDirection))
+	lefty=col+int(a*math.Sin(leftDirection))
+	rightx=row+int(a*math.Cos(rightDirection))
+	righty=col+int(a*math.Sin(rightDirection))
+	return leftx,lefty,rightx,righty
+ }
 
 //Calculate a box's score using SV = WT×TV +WN×NV
 func calculateScore(B box, WT, WN float64) float64 {
 	return WT*B.trailChemo + WN*B.foodChemo
 }
 
-func FindMax(score1, score2, score3 float64) int {
-	var slice = []float64{score1, score2, score3}
-	max := math.Max(score1, math.Max(score2, score3))
-
-	for i := range slice {
-		if slice[i] == max {
-			return i
-		}
-	}
-	panic("Error when finding Max value")
-}
-
-//InField takes a GameBoard and i/j indices.  It returns true if (i,j) is a valid entry
+//InField takes a the numRows and numCols of a matrix and i/j indices.  It returns true if (i,j) is a valid entry
 //of the board.
-func InField(M multiAgentMatrix, i, j int) bool {
-	numRows := len(M)
-	numCols := len(M[0])
+func InField(numRows,numCols, i, j int) bool {
 	if i < 0 || j < 0 || i >= numRows || j >= numCols {
 		return false
 	}
