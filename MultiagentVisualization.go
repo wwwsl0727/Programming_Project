bb7db6152408d2890b4d78ebe109987936b743ed
liujerry1997 <liujerry@bu.edu>
2019-11-28 17:12:58 -0500

    -Change files names: visualization.go in Maze changes to SPinMaze_visualization.go -Update Multiagent visualization -Add boardDrawer.go to file (similar to Enzo's SPinMaze_visualization.go)

--- MultiagentVisualization.go
+++ MultiagentVisualization.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"image"
+	"image/png"
+	"os"
+
+	"code.google.com/p/draw2d/draw2d"
 )
+
+type drawer struct {
+	gc     *draw2d.ImageGraphicContext
+	img    image.Image
+	width  int
+	height int
+}
 
 //
 // func DrawGameBoards(boards []Board, cellWidth int) []image.Image {
@@ -14,34 +25,72 @@ import (
 // 	return imageList
 // }
 
-func DrawGameBoard(board multiAgentMatrix, cellWidth int) image.Image {
+func OutPNGPicture(boards []multiAgentMatrix, cellWidth int, CN float64) {
+	imglist := DrawGameBoards(boards, 5, CN)
+	// Images to severial PNG(imglist, "outputFile")
+	for i := range imglist {
+		f, err := os.Create("i" + "Prisoners.png")
+		if err != nil {
+			panic(err)
+		}
+		png.Encode(f, imglist[i])
+	}
+}
+
+func DrawGameBoards(boards []multiAgentMatrix, cellWidth int, CN float64) []image.Image {
+	numGenerations := len(boards)
+	imageList := make([]image.Image, numGenerations)
+	for i := range boards {
+		imageList[i] = DrawGameBoard(boards[i], cellWidth, CN) //CN is here to check if it's good food or bad food.
+	}
+	return imageList
+}
+
+func DrawGameBoard(board multiAgentMatrix, cellWidth int, CN float64) image.Image {
 	height := len(board) * cellWidth
 	width := len(board[0]) * cellWidth
-	c := CreateNewCanvas(width, height)
 
 	// declare colors
 	// black := MakeColor(0, 0, 0)
-	black := MakeColor(0, 0, 0)
-	yellow := MakeColor(255, 255, 0)
-	white := MakeColor(255, 255, 255)
-	/*
-		//set the entire board as black
-		c.SetFillColor(gray)
-		c.ClearRect(0, 0, height, width)
-		c.Clear()
-	*/
+	grey := MakeColor(211, 211, 211)  //background
+	red := MakeColor(0, 0, 0)         //bad food
+	green := MakeColor(0, 255, 255)   //good food
+	yellow := MakeColor(255, 255, 0)  //Agent
+	white := MakeColor(255, 255, 255) //light
+
+	c := CreateNewMazes(width, height) //Create new drawer for board
+	//set the entire board as grey
+	c.SetFillColor(grey)
+	// c.ClearRect(0, 0, height, width)
+	c.Clear()
 
 	// draw the grid lines in white
-	c.SetStrokeColor(white)
-	DrawGridLines(c, cellWidth)
+	// c.SetStrokeColor(white)
+	// DrawGridLines(c, cellWidth)
 
-	// fill in colored squares
+	// fill in colored squares and draw
 	for i := range board {
 		for j := range board[i] {
-			if board[i][j].IsFood {
-				c.SetFillColor(black)
-			} else if board[i][j].IsAgent {
+			if board[i][j].IsAgent {
 				c.SetFillColor(yellow)
+				c.Circle(float64(i), float64(j), 1.0) //draw a circle of radius 1
+				c.Fill()
+
+			} else if board[i][j].IsFood {
+				if board[i][j].foodChemo == CN {
+					c.SetFillColor(green)                         //good food
+					c.Rectangle(float64(i), float64(j), 1.0, 1.0) //draw a square of length 1
+					c.Fill()
+				} else if board[i][j].foodChemo == 0 {
+					c.SetFillColor(red) // bad food
+					c.Rectangle(float64(i), float64(j), 1.0, 1.0)
+					c.Fill()
+				}
+			}
+			if board[i][j].haslight {
+				c.SetFillColor(white)
+				c.Rectangle(float64(i), float64(j), 1, 1)
+				c.Fill()
 			}
 			x := j * cellWidth
 			y := i * cellWidth
